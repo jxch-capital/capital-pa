147d00acc673e92bd5444e8a066b49e08c166dfa
jxch <15553598935@163.com>
2023-07-03 11:18:04 +0800

    init

---
+++ src/main/java/org/jxch/capital/pa/PAApp.java
@@ -0,0 +1,11 @@
+package org.jxch.capital.pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PAApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PAApp.class, args);
+    }
+}
